8fb93a79e6b3bbf0c5f00cbc1f9540543951b79e
Frawst93 <139417047+Frawst93@users.noreply.github.com>
2025-04-08 15:44:15 -0400

    Update cl_blueprints.lua

--- client/cl_blueprints.lua
+++ client/cl_blueprints.lua
@@ -12,7 +12,7 @@ RegisterNetEvent('mainframe:blueprints:startMissionClient', function(missionKey,
     end
 
     activeMission = true
-    QBCore.Functions.Notify("Mission started: Escort the vehicle to its destination.", "primary")
+    QBCore.Functions.Notify("Mission started: Check your GPS and follow objectives.", "primary")
 
     if missionKey == "smuggler_escort" then
         StartEscortMission(coords)
@@ -36,7 +36,6 @@ function StartEscortMission(spawnCoords)
     TaskVehicleDriveWander(convoyDriver, convoyVeh, 25.0, 786603)
     SetVehicleDoorsLocked(convoyVeh, 1)
 
-    -- Mark destination
     local destination = vector3(1386.62, 3609.82, 34.89)
     local blip = AddBlipForCoord(destination)
     SetBlipRoute(blip, true)
@@ -45,11 +44,8 @@ function StartEscortMission(spawnCoords)
     QBCore.Functions.Notify("Escort the convoy and ensure it reaches the drop-off point.", "info")
 
     CreateThread(function()
-        while #(GetEntityCoords(convoyVeh) - destination) > 15.0 do
-            Wait(1000)
-        end
+        while #(GetEntityCoords(convoyVeh) - destination) > 15.0 do Wait(1000) end
 
-        -- Clean up
         DeleteVehicle(convoyVeh)
         RemoveBlip(blip)
         TriggerServerEvent('mainframe:blueprints:completeEscortMission')
@@ -58,5 +54,69 @@ function StartEscortMission(spawnCoords)
     end)
 end
 
--- TODO: Implement StartWarehouseRaid()
+function StartWarehouseRaid(coords)
+    QBCore.Functions.Notify("Travel to the warehouse and breach the back door.", "info")
+
+    local blip = AddBlipForCoord(coords)
+    SetBlipSprite(blip, 539)
+    SetBlipColour(blip, 1)
+    SetBlipRoute(blip, true)
+    SetBlipRouteColour(blip, 1)
+
+    local breached = false
+
+    CreateThread(function()
+        while not breached do
+            Wait(0)
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            if #(playerCoords - coords) < 3.5 then
+                DrawText3D(coords.x, coords.y, coords.z, "[E] Shoot lock to enter")
+                if IsPedArmed(PlayerPedId(), 6) and IsControlJustReleased(0, 38) then -- E key
+                    breached = true
+                    RemoveBlip(blip)
+                    QBCore.Functions.Notify("You've breached the door, hostiles inside!", "warning")
+                    SpawnWarehouseGuards(coords)
+                end
+            end
+        end
+    end)
+end
+
+function SpawnWarehouseGuards(center)
+    local pedModel = `g_m_y_mexgoon_02`
+    RequestModel(pedModel)
+    while not HasModelLoaded(pedModel) do Wait(0) end
+
+    local spawnOffsets = {
+        vector3(2, 1, 0), vector3(-2, 1, 0), vector3(1, -2, 0)
+    }
+
+    for _, offset in pairs(spawnOffsets) do
+        local pos = center + offset
+        local ped = CreatePed(4, pedModel, pos.x, pos.y, pos.z, 90.0, true, false)
+        GiveWeaponToPed(ped, `weapon_microsmg`, 250, false, true)
+        TaskCombatPed(ped, PlayerPedId(), 0, 16)
+    end
+
+    CreateThread(function()
+        Wait(10000)
+        QBCore.Functions.Notify("Collect the blueprint crate before reinforcements arrive!", "success")
+        TriggerServerEvent('mainframe:blueprints:completeWarehouseMission')
+        activeMission = false
+    end)
+end
+
+function DrawText3D(x, y, z, text)
+    SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    DrawText(_x, _y)
+    local factor = (string.len(text)) / 370
+    DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 0, 0, 0, 100)
+end
+
 -- TODO: Implement StartIntelTheft()
